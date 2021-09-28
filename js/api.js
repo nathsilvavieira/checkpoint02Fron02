e5887cfbb68847e8b04f4239a439461ee7bad0d3
Nathalia Vieira <naahviera@gmail.com>
2021-09-27 22:25:32 -0300

    adicionando comentarios codigo

--- js/api.js
+++ js/api.js
@@ -1,20 +1,20 @@
-
+/* Função para carregamento da Janela */
 function carregandoJanela() {
     tempo = 0;
-    let loading = document.getElementById('loading');
+    let loading = document.getElementById('loading'); /* pegando id  */
     console.log(loading)
 
-    setTimeout(() => {
+    setTimeout(() => { /* função que será setada após tempo indicado e aplicará função que retirará o elemento do layout da tela */
         loading.style.display = 'none';
     }, 2000);
 }
 
-let sectionTarefas = document.getElementById('secao-tarefas');
-
-function criarTarefa(descricao, id) {
-    const artigo = document.createElement('article');
-    artigo.setAttribute('id', 'visualizar');
-    artigo.innerHTML =
+let sectionTarefas = document.getElementById('secao-tarefas'); /* pegando id  */
+/* Função que cria tarefas */
+function criarTarefa(descricao, id) { /* passando parametros da função */
+    const artigo = document.createElement('article'); /* criando elemento article */
+    artigo.setAttribute('id', 'visualizar'); /* adicionando atributos na tag article */
+    artigo.innerHTML = /* adicionando elementos dentro da tag article no html */
      ` 
     <div class="section__box-icons">
     </div>
@@ -26,26 +26,26 @@ function criarTarefa(descricao, id) {
         </div>
     
     `
-    return artigo;
+    return artigo; /* retornando tag article criada */
 }
 
-
+/* Funcao assincrona para requisitar tarefas na API */
 async function requisitarTarefas() {
-    try {
-        const resquisicao = await (await fetch('https://jsonplaceholder.typicode.com/todos/')).json();
-        for (let i = 0; i < resquisicao.length; i++) {
-            let novaTarefa = criarTarefa(resquisicao[i].title, resquisicao[i].id)
-            if(resquisicao[i].completed == true) {
-                novaTarefa.children[1].children[0].classList.toggle('tachado');
-                novaTarefa.style.opacity = "0.87";
-                novaTarefa.style.filter = "grayscale(0.75)";
+    try { /* pedindo para função tentar executar os comandos  */
+        const resquisicao = await (await fetch('https://jsonplaceholder.typicode.com/todos/')).json(); /* pausa na função assincrona para depois retornar a ela novamente */
+        for (let i = 0; i < resquisicao.length; i++) { /* looping de repetição para executar enquanto houver itens no requisição */
+            let novaTarefa = criarTarefa(resquisicao[i].title, resquisicao[i].id) /* criando nova tarefa */
+            if(resquisicao[i].completed == true) { /* se valor da requisição for true retornará bloco de tarefas */
+                novaTarefa.children[1].children[0].classList.toggle('tachado'); /* criando card e adicionando classe */
+                novaTarefa.style.opacity = "0.87"; /*definindo opacidade do card */
+                novaTarefa.style.filter = "grayscale(0.75)"; /* aplicando filtro */
             }
-            sectionTarefas.appendChild(novaTarefa);
+            sectionTarefas.appendChild(novaTarefa); /*adicionando dinamicamente elemento filho dentro do elemento pai no html */
         }
-    } catch (erro) {
-        console.log(erro.name);
-        return alert(Error("Falha em requisitar os dados. \n Verifique sua conexão com a Internet ou tente novamente mais tarde")); 
+    } catch (erro) { /* tratamento de erros para evitar que programa para em tempo de execução */
+        console.log(erro.name); /* mostra no console erro */
+        return alert(Error("Falha em requisitar os dados. \n Verifique sua conexão com a Internet ou tente novamente mais tarde")); /* mensagem de erro para usuario */
     }
 }
 
-requisitarTarefas();
+requisitarTarefas(); /**chamando função requisitarTarefas */
--- js/app.js
+++ js/app.js
@@ -1,63 +1,63 @@
-//VARIÁVEIS
+// DECLARANDO AS VARIÁVEIS
 
-let botaoSalvar = document.getElementById('btn-salvar');
+let botaoSalvar = document.getElementById('btn-salvar'); /* PEGANDO ELEMENTOS PELO ID CORRESPONDENTE */
 let form = document.getElementById('formulario');
 let sectionTarefas = document.getElementById('secao-tarefas');
 
 /* gerando data atual */
-let data = new Date();
-let dia = String(data.getDate()).padStart(2, '0');
-let mes = String(data.getMonth() + 1).padStart(2, '0');
-let ano = data.getFullYear();
-let dataAtual = ano + '-' + mes + '-' + dia;
+let data = new Date(); /**INSTANCIANDO UM OBJETO DO TIPO DATA */
+let dia = String(data.getDate()).padStart(2, '0'); /** metodo pega data transformando data em string e passando metodo padStart para receber caracteres */
+let mes = String(data.getMonth() + 1).padStart(2, '0'); /**metodo pega mes */
+let ano = data.getFullYear(); /** metodo que pega todo ano */
+let dataAtual = ano + '-' + mes + '-' + dia; /**concatenando elementos */
 
-const localStorageTarefas = JSON.parse(localStorage.getItem('arrayTarefas'));
-const minhasTarefas = localStorage.getItem('arrayTarefas') !== null ? localStorageTarefas : [];
+const localStorageTarefas = JSON.parse(localStorage.getItem('arrayTarefas')); /** trasnformando json em js, acessando local storage  e pegando arrayTarefas */
+const minhasTarefas = localStorage.getItem('arrayTarefas') !== null ? localStorageTarefas : []; /**verificando se arrayTarefas é vazio  */
 
-const localStorageIdTarefas = JSON.parse(localStorage.getItem('idsTarefas'));
-let indexTarefas = localStorage.getItem('idsTarefas') !== null ? localStorageIdTarefas : [];
-
-for (objTarefa of minhasTarefas) {//leitura de itens no localStorage
-    let novoTarefa = criarTarefa(objTarefa.dataCriacao, objTarefa.dataLimite, objTarefa.descricao, objTarefa.idTarefa);
-    sectionTarefas.appendChild(novoTarefa);
+const localStorageIdTarefas = JSON.parse(localStorage.getItem('idsTarefas')); /** trasnformando json em js, acessando local storage  e pegando idsTarefas */
+let indexTarefas = localStorage.getItem('idsTarefas') !== null ? localStorageIdTarefas : []; /**verificando se idsTarefas é vazio  */
+/**LENDO ITENS DO LOCALSTORAGE */
+for (objTarefa of minhasTarefas) {/**for que percorre elementos minhasTarefas */
+    let novoTarefa = criarTarefa(objTarefa.dataCriacao, objTarefa.dataLimite, objTarefa.descricao, objTarefa.idTarefa); /** alocando elementos dentro do objTarefas */
+    sectionTarefas.appendChild(novoTarefa); /**adicionando objTarefas filho dentro da sectionTarefas  */
 }
-
-let iconsChecked = document.querySelectorAll('[data-click-checked]');
-iconsChecked.forEach(el => el.addEventListener('click', (e) => {
-    e.preventDefault();
+/**ICONES CLICAVEIS */
+let iconsChecked = document.querySelectorAll('[data-click-checked]'); /**pegando elemento */
+iconsChecked.forEach(el => el.addEventListener('click', (e) => { /**percorre elemento e escuta evento click */
+    e.preventDefault();/**previnindo comportamento padrao do elemento */
     let divInput = el.parentElement;
     let artigo = divInput.parentElement
     let paragrafo = divInput.parentElement.children[2].children[0];
-    paragrafo.classList.toggle('tachado');
-    artigo.classList.toggle('artigo-checked');
+    paragrafo.classList.toggle('tachado'); /**adicionando classe tachado */
+    artigo.classList.toggle('artigo-checked'); /**adicionando artigo-checked */
 }))
 
-
-function aplicarBackground(el, article) {
-    let paletaCores = article.children[1];
-    let input = article.children[0].children[1];
+/**APLICANDO BACKGROUND DE MULTIPLAS CORES */
+function aplicarBackground(el, article) { /**função que recebe dois parametros  */
+    let paletaCores = article.children[1]; /**definindo elemento da paleta */
+    let input = article.children[0].children[1]; /**definindo input das cores */
    
-        return function () {
-            article.style.backgroundColor = el.value;
-            paletaCores.removeChild(paletaCores.firstChild);
-            input.disabled = false;
+        return function () { /**chmando a função anonima que seta as cores */
+            article.style.backgroundColor = el.value; /**background-color receberar valor do el que será alterado no css */
+            paletaCores.removeChild(paletaCores.firstChild); /**removendo cor anterior */
+            input.disabled = false; /**desabilitando input  */
         }
 }
 
-var cores = ["#FFFCE8", "#DEDBD8", "#D8FFDD", "#CCFFFF","#E5FCFF", "#CCCCFF"];
-function criarPaleta(artigo) {
-    let divPaleta = document.createElement('div');
-    divPaleta.setAttribute('class', 'child-paleta');
-    cores.forEach(function (cor) {
+var cores = ["#FFFCE8", "#DEDBD8", "#D8FFDD", "#CCFFFF","#E5FCFF", "#CCCCFF"]; /**criando paleta de cores */
+function criarPaleta(artigo) { /**FUNÇÃO QUE CRIA PALETA DE CORES */
+    let divPaleta = document.createElement('div'); /**pegando elemento div */
+    divPaleta.setAttribute('class', 'child-paleta'); /**setando atributos na div */
+    cores.forEach(function (cor) { /**percorrendo divPaleta e aplicando atributos e valores dinamicamente  */
         var button = document.createElement('button');
         button.setAttribute('class', 'btn-cor');
         button.value = cor;
         button.type = 'button';
         button.style.backgroundColor = cor;
-        button.addEventListener('click', aplicarBackground(button, artigo));
-        divPaleta.appendChild(button);
+        button.addEventListener('click', aplicarBackground(button, artigo));/**criando evento escutador que iniciará quando clicado */
+        divPaleta.appendChild(button); /**inserindo elemento filho no html */
     });
-    return divPaleta;
+    return divPaleta; /**pedindo para retornar divPaleta */
 }
 
 let iconsColor = document.querySelectorAll('[data-click-color]');
